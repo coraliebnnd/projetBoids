da372ea36319e94cd7b9f65a9352aa1e10eaa256
coralie <coralie.bnnd@gmail.com>
2024-04-09 12:30:14 +0200

    mise en place camera

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,11 @@
 #include <imgui.h>
 #include <cstddef>
 #include <cstdlib>
-#include <iostream>
 #include "glm/fwd.hpp"
 #define DOCTEST_CONFIG_IMPLEMENT
 #include <VAO.hpp>
+#include "../src-common/glimac/TrackballCamera.hpp"
 #include "../src-common/glimac/common.hpp"
-#include "../src-common/glimac/default_shader.hpp"
 #include "../src-common/glimac/sphere_vertices.hpp"
 #include "Boid.hpp"
 #include "GroupBoid.hpp"
@@ -14,30 +13,9 @@
 #include "doctest/doctest.h"
 #include "glm/ext/matrix_clip_space.hpp"
 #include "glm/ext/matrix_transform.hpp"
-#include "glm/fwd.hpp"
-#include "glm/gtc/random.hpp"
 #include "glm/gtc/type_ptr.hpp"
 #include "glm/matrix.hpp"
 #include "glm/trigonometric.hpp"
-#include "p6/p6.h"
-
-struct BoidProgram {
-    p6::Shader m_Program;
-
-    GLint uMVPMatrix;
-    GLint uMVMatrix;
-    GLint uNormalMatrix;
-    GLint uTexture;
-
-    BoidProgram()
-        : m_Program{p6::load_shader("../src/shaders/3D.vs.glsl", "../src/shaders/tex3D.fs.glsl")}
-    {
-        uMVPMatrix    = glGetUniformLocation(m_Program.id(), "uMVPMatrix");
-        uMVMatrix     = glGetUniformLocation(m_Program.id(), "uMVMatrix");
-        uNormalMatrix = glGetUniformLocation(m_Program.id(), "uNormalMatrix");
-        uTexture      = glGetUniformLocation(m_Program.id(), "uTexture");
-    }
-};
 
 float cohesion        = 0.3f;
 float separation      = 0.15f;
@@ -62,6 +40,20 @@ void sliders()
     ImGui::End();
 }
 
+struct BoidProgram {
+    p6::Shader m_Program;
+
+    GLint uMVPMatrix;
+    GLint uMVMatrix;
+    GLint uNormalMatrix;
+    GLint uTexture;
+
+    BoidProgram()
+        : m_Program{p6::load_shader("../src/shaders/3D.vs.glsl", "../src/shaders/tex3D.fs.glsl")}, uMVPMatrix(glGetUniformLocation(m_Program.id(), "uMVPMatrix")), uMVMatrix(glGetUniformLocation(m_Program.id(), "uMVMatrix")), uNormalMatrix(glGetUniformLocation(m_Program.id(), "uNormalMatrix")), uTexture(glGetUniformLocation(m_Program.id(), "uTexture"))
+    {
+    }
+};
+
 int main()
 {
     GroupBoid group;
@@ -72,6 +64,9 @@ int main()
     // Actual application code
     auto ctx = p6::Context{{.title = "Simple-p6-Setup"}};
     ctx.maximize_window();
+
+    /************ CAMERA ***************/
+    TrackballCamera trackBallCamera;
 
     // chargement des shaders
     BoidProgram boidProgram{};
@@ -107,11 +102,43 @@ int main()
 
     vao.debind();
 
-    glm::mat4 viewMatrix = glm::translate(glm::mat4{1.f}, glm::vec3{0., 0., -1});
+    // last position souris
+    float lastX = 0;
+    float lastY = 0;
 
     // Declare your infinite update loop.
     ctx.update = [&]() {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        /************ CAMERA ***************/
+        ctx.mouse_dragged = [&](p6::MouseDrag drag) {
+            float deltaX = drag.position.x - lastX;
+            float deltaY = drag.position.y - lastY;
+
+            if (lastX != 0 && lastY != 0)
+            {
+                // camera autour d'un objet
+                trackBallCamera.rotateLeft(-deltaX * 50.f);
+                trackBallCamera.rotateUp(deltaY * 50.f);
+            }
+
+            lastX = drag.position.x;
+            lastY = drag.position.y;
+        };
+
+        if (!ctx.mouse_button_is_pressed(p6::Button::Right))
+        {
+            lastX = 0;
+            lastY = 0;
+        }
+
+        // camera autour d'un object
+        ctx.mouse_scrolled = [&](p6::MouseScroll scroll) {
+            trackBallCamera.moveFront(-scroll.dy);
+        };
+
+        /*** VIEW MATRIX ***/
+        glm::mat4 viewMatrix = trackBallCamera.getViewMatrix();
 
         // vao
         vao.bind();
@@ -123,10 +150,6 @@ int main()
         // texture
         // glUniform1i(boidProgram.uTexture, 0);
 
-        // matrices
-        // Calculer la matrice modèle lune
-        // glm::mat4 modelMatrix{1.f};
-
         // // texture
         // glActiveTexture(GL_TEXTURE0);
         // glBindTexture(GL_TEXTURE_2D, textureLune);
@@ -134,37 +157,28 @@ int main()
         sliders();
         group.moveBoids(cohesion, separation, forceCohesion, forceSeparation, mur, forceMur, alignement, forceAlignement);
 
-        // ctx.background(p6::NamedColor::AshGrey);
+        ctx.background(p6::NamedColor::DarkGray);
         for (Boid& boid : group.getGroup())
         {
-            // ctx.circle(
-            //     p6::Center{boid.getPosition()},
-            //     p6::Radius{0.01f}
-            // );
+            /**********MATRICES BOID**********/
 
-            // glm::mat4 modelMatrixBoid = glm::rotate(glm::mat4(1.0f), ctx.time(), axes[i]);
+            // matrix model
             glm::mat4 modelMatrixBoid = glm::translate(glm::mat4(1.0f), boid.getPosition());
             modelMatrixBoid           = glm::scale(modelMatrixBoid, glm::vec3(0.01, 0.01, 0.01));
 
-            // Calculer la matrice modèle-vue lune
+            // matrix model-vue
             glm::mat4 modelViewMatrixBoid = viewMatrix * modelMatrixBoid;
 
-            // Calculer la matrice MVP lune
+            // matrix MVP
             glm::mat4 MVPMatrixBoid = projMatrix * modelViewMatrixBoid;
 
             glUniformMatrix4fv(boidProgram.uMVMatrix, 1, GL_FALSE, glm::value_ptr(modelViewMatrixBoid));
             glUniformMatrix4fv(boidProgram.uNormalMatrix, 1, GL_FALSE, glm::value_ptr(glm::transpose(glm::inverse(modelViewMatrixBoid))));
             glUniformMatrix4fv(boidProgram.uMVPMatrix, 1, GL_FALSE, glm::value_ptr(MVPMatrixBoid));
 
-            // dessin boid
+            // draw boid
             glDrawArrays(GL_TRIANGLES, 0, vertices.size());
         }
-
-        // ctx.fill = {0., 0., 0., 0.};
-        // ctx.square(
-        //     p6::Center{0, 0},
-        //     p6::Radius{1}
-        // );
 
         vao.debind();
     };
